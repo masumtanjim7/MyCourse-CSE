9bcee2dcdf0582d03ba3672d56e624ed21f0be02
MasumTanjim7 <masumbillah19999@gmail.com>
2024-05-24 04:34:04 +0600

    Masum

--- Advance Programming - CSE 331 & 332/MediatorExercise/MediatorExercise/src/com/company/CollegueInterface.java
+++
@@ -1,5 +0,0 @@
-package com.company;
-
-public interface CollegueInterface {
-    void receiveMessage(String message);
-}
--- Advance Programming - CSE 331 & 332/MediatorExercise/MediatorExercise/src/com/company/MediatorInterface.java
+++
@@ -1,5 +0,0 @@
-package com.company;
-
-public interface MediatorInterface {
-    void sendMessage(String message, CollegueInterface user);
-}
